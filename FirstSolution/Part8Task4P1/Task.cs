8e3798f5e71d0da91fea6d4f57e93a6e783f5fae
alexkulesh <al.kulesh.box@mail.ru>
2021-11-30 18:02:42 +0300

    Part8 all tasks done

---
+++ FirstSolution/Part8Task4P1/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Part8Task4P1
+{
+    
+    // Пользователь вводит число. Выведите n строк, добавляя следующее
+    // натуральное число на каждой строке. Используйте цикл while
+    
+    public class Task
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            string num = n.ToString();
+            string t = "";
+            
+            while (i <= num.Length - 1)
+            {
+                t += num[i];
+                Console.WriteLine(t);
+                i++;
+            }
+        }
+    }
+}
---
+++ FirstSolution/Part8Task4P2/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Part8Task4P2
+{
+    
+    // Пользователь вводит число. Выведите n строк, добавляя следующее
+    // натуральное число на каждой строке. Используйте цикл for
+    
+    public class Task
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            string num = n.ToString();
+            string t = "";
+            
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                t += num[i];
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
