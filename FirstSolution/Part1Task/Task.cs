2aa418438ec687874a6cbe8ba887965247da5839
alexkulesh <al.kulesh.box@mail.ru>
2021-11-30 13:04:01 +0300

    Part1 Task done

---
+++ FirstSolution/Part1Task/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Part1Task
+{
+    public class Task
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 100, 200 };
+            
+            for (int i = 1; i < 4; i++)
+            {
+                 Console.WriteLine(array[1]);
+            }
+        }
+    }
+}
